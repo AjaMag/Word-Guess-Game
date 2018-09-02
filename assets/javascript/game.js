0881764365a81f73744dc7b81bf58c752d0502fb
Phoenix <aja.magdaleno@gmail.com>
2018-09-01 18:16:53 -0700

    working on checkLetter function

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -19,6 +19,7 @@ document.querySelector('#loses').innerHTML = loses;
 let splitWord = [];
 let hiddenArr = [];
 let pGuess = [];
+ 
 //declares the start function
 function start() {
    //The compChooses() function here makes the computer choose a randWord from listOfWords..
@@ -28,8 +29,8 @@ function start() {
     randWord = compChooses(listOfWords);
     console.log(randWord);
 //The count of guessesLeft is written on the page with innerHTML
-    let gLeft = 10;
-    document.querySelector('#guessesLeft').innerHTML = gLeft;
+    gLeft = 12;
+    document.querySelector('#guessesR').innerHTML = gLeft;
 
 //this splits the letters of randWord (a string) into an array, called splitWord
     splitWord = randWord.split('');
@@ -39,41 +40,64 @@ function start() {
         hiddenArr = splitWord.map(function () {
             return '_';
             });
-
+        console.log(hiddenArr);
     };
-//this calls the hiddenWordArray function, above
+    //this calls the hiddenWordArray function
     hiddenWordArray();
-//this writes the hiddenArr (which is the randWord with all letters replaced with underscores) in html on the pg
-    document.querySelector('#hiddenWord').innerHTML = hiddenArr;
+    //this join method returns the array without commas seperating each letter, and .innerHTML writes it on the page
+    document.querySelector('#hiddenWord').innerHTML = hiddenArr.join(" ");
 };
-//this calls the start function, above
+//when page loads this calls the start function, above
 start();
 
-// 2.) When user enters any letter key into the textbox, the below will run    
-document.onkeyup = function(event){
-// changes all letter inputs to Lower Case so it matches the case in all my arrays
+// 2.) Checks if player guess is a repeated letter
+// function isRepeatedKey(userGuess) {
+//         var isInArray = pGuess.indexOf(userGuess);
+//         return isInArray < 0 ? false : true;
+//     };
+// 3.) When user enters letters ONLY into the textbox, the below will run 
+document.addEventListener('keyup', function(event) {
+    if (event.which >= 65 && event.which <= 90) {
+       console.log(event.which)
+    // changes all letter inputs to Lower Case so it matches the case in all my arrays
     let letter = event.key.toLowerCase();
-//creates an array of player guesses called pGuess, which will add on each letter the user enters 
-    pGuess.push(letter);
-//this loop decides if any user-entered letter in the pGuess array is the same as any element in splitWord array    
-    for (i = 0; i < pGuess.length; i++) {  
-        
-  if(splitWord.indexOf(letter) {
-
-  }
-)
-            
-        } 
-//The above if / else says that if the player's guessed letter is in the word, 
-//then replace the appropriate underscore in hiddenArr with the appropriate letter of splitWord.         
-
-//the below says if player enters a letter that is not in the word, then guesses left decreases by
-// 1 and is written in html on the page each time.        
-    if (splitWord.indexOf(letter) < 0) {
-        gLeft--;
-        document.querySelector('#guessesLeft').innerHTML = `${gLeft}`; 
+    // creates an array of player guesses called pGuess, which will add on each letter the user enters 
+    pGuess.push(letter)
+    console.log(pGuess);
+    document.querySelector('#guessedL').innerHTML = pGuess;
+    // decides whether a letter input in the pGuess array is in the splitWord array 
+    function checkLetter() {
+        let dashes_temp = ""
+        let count = 0
+        let dashes = ""
+        for (let i = 0; i < splitWord.length; i++) { 
+            dashes = dashes + "_"
+            if (splitWord[i] === letter) {
+                console.log("needs replacing")
+                dashes_temp = dashes_temp + i + "" 
+        }
+            else {
+                console.log("stays as dash")
+                dashes_temp = dashes_temp + dashes[count] + ""
+            }    
+        }
+        count++
+        console.log(dashes_temp) 
     }
+    checkLetter();
+}
+else {
+    alert("The key you entered is not a valid choice... Please choose a letter")
 };
+})
+   
+
+//     //the below says if player enters a letter that is not in the word, then guesses left decreases by
+// // 1 and is written in html on the page each time.        
+// // if (splitWord.indexOf(letter) < 0 {
+// //     gLeft--;
+// //     document.querySelector('#guessesLeft').innerHTML = `${gLeft}`; 
+// // })
 
 // winGame: function() {
 //   //if player has entered all letters in the Word
