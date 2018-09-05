b407cedaf17d214f3328e96ca4c22a8ff78ca261
Phoenix <aja.magdaleno@gmail.com>
2018-09-04 21:33:29 -0700

    "FINISHED a complete loop of gameplay!'

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,4 +1,3 @@
-
 //The list of words from which one word will be chosen randomly for each round of play
 let listOfWords = ["sufficiently", "conditional", "arbitrary", "conceptual", "transitory", "barter", "antithesis", "longitude", "latitude", "victorious","recharged", "instrumentation", "constraint", "groundwater", "transphobia", "isolationist", "infringement", "objective", "varietal", "configuration", "humanism", "philosophy", "tolerance", "acquisitions", "biodiversity",
     "snowboard", "subjective", "coordinated", "strategize", "controversy", "legendary", "artfully", "antibiotics", "kombucha", "examinations", "departmental", "terminology", "gentleman", "reproduce", "ethical", "roommates", "relaxing", "spokesperson", "activists", "pizza", "encouraging", "assembly", "restructuring", "finals", "simulations", "physics", "creations", "hypothesis",
@@ -7,17 +6,20 @@ let listOfWords = ["sufficiently", "conditional", "arbitrary", "conceptual", "tr
     ]; 
 
 //When page loads, #1 below will run 
-// 1.) guesses left and randWord are given an empty string in global scope to be used in later functions
+// 1.) guesses left and randWord are given an empty string in global scope to be used later
 let gLeft = "";
 let randWord = "";
-//wins and losses are set to zero and written in html.
-let wins = 0;
-document.querySelector('#wins').innerHTML = wins;
-let loses = 0;
-document.querySelector('#loses').innerHTML = loses;
 //Declaring some globals to be used in start()
 var a;
 var answerArr = []; 
+
+//this is what runs when the player loses
+function gameLose() {
+    if (gLeft == 0)  {
+        document.getElementById('comment').innerHTML ="Sorry, you lost.. the correct word was: " + randWord;
+        document.getElementById('gamePlay').innerHTML = "Refresh page another round!"
+    }
+}
 //declares the start function
 function start() {
    //The compChooses() function here makes the computer choose a randWord from listOfWords..
@@ -36,114 +38,65 @@ function start() {
         a = answerArr.join(" ");
 //the above join method returns the array without commas seperating each letter, and the below .innerHTML writes it on the page
     document.querySelector('#hiddenWord').innerHTML = a;
-    console.log(a)    
+    console.log(a)
 };
 //when page loads this calls the start function, above
 start();
 
-//Declaring some globals to be used in the key up function below
+//Declaring some globals to be used in the functions below
 var pGuess = [];
 var count = 0;
 var x ;
+var letter;
 // 2.) When the user hits keys, the below "keyup" will run 
 document.addEventListener('keyup', function(event) {
     // this first "if" condition looks only for letter keys
     if (event.which >= 65 && event.which <= 90) {
-        // changes all letter inputs to Lower Case so it matches the case in all my arrays
-        var letter = event.key.toLowerCase();
+        // changes all letter inputs to Lower Case so it matches the case in all arrays
+        letter = event.key.toLowerCase();
         //for the array of player guesses called pGuess, this adds each letter to the end of the pGuess array 
         pGuess.push(letter)
         //this writes all player guesses onto html
         document.querySelector('#guessedL').innerHTML = pGuess;
+        
         //this function decides whether the player's letter input is in the randWord or not 
         function checkLetter() {
             for (let i = 0; i < randWord.length; i++) {
                 if (randWord[i] === letter) {
                     answerArr[i] = letter;
-                    document.getElementById('comment').innerHTML = "Your guess is correct!"
-                } else {
-                    document.getElementById('comment').innerHTML = "Oops!"
+                    //this writes a comment for the user on the page
+                    document.getElementById('comment').innerHTML = "Your guess is a correct letter!"
+                    //this writes the answerArr updated with user-chosen letters onto the page
+                    document.getElementById('hiddenWord').innerHTML = answerArr.join(" ");
                 }
-            }   
-        document.getElementById('hiddenWord').innerHTML = answerArr.join(" ");
-        }
-        checkLetter();
+                if (answerArr.indexOf("_") === -1) {
+                    document.getElementById('comment').innerHTML = "All correct! Congratulations! You win!"
+                    document.getElementById('gamePlay').innerHTML = "Refresh page for another round!"
+                }
+            }
+            //this says if the player chooses a letter NOT in the randWord..
+            if (randWord.indexOf(letter) === -1)  {
+                //this writes a comment for the user on html
+                document.getElementById('comment').innerHTML = "Oops... that letter is not in the word.  Guess again!"
+                // ...then player's guesses left is reduced by 1
+                gLeft--
+            }    
+        } 
+     //runs the checkLetter()    
+     checkLetter();    
+     gameLose();
+    //writes the new amount of guesses left on the page
+    document.getElementById('guessesR').innerHTML = gLeft;        
     }
-    //if user hit a key that is a non-letter this will run
+//this else condition is for when user enters a non-letter key
     else {
         alert("The key you entered is not a valid choice... Please choose a letter")
-    };
+    };    
 })
-     // if (var x = false) {
-        //     
-        //     
+// Checks if player guess is a repeated letter
+    // var isInArray = pGuess.indexOf(letter);
+    //     console.log("Already Picked")
 
-//     //the below says if player enters a letter that is not in the word, then guesses left decreases by
-// // 1 and is written in html on the page each time.        
-// // if (splitWord.indexOf(letter) < 0 {
-// //    
-// //      
-// // })
 
-// winGame: function() {
-//   //if player has entered all letters in the Word
-//   alert("You won! The correct word is: " + word([i]));
-//   //wins++
-//   //runs start function
-// },
 
-// loseGame: function() {
-//   if count = 0  
-//   alert("Sorry, you lost.. the correct word was: " + word([i]));
-//   //losses++
-//   //runs start function  
-// },
-// 
-// }
-// 2.) Checks if player guess is a repeated letter
-// function isRepeatedKey(userGuess) {
-//         var isInArray = pGuess.indexOf(userGuess);
-//         return isInArray < 0 ? false : true;
-//     };
-// let splitWord = [];
-// let hiddenArr = [];
-//this splits the letters of randWord (a string) into an array, called splitWord
-//     splitWord = randWord.split('');
-//     console.log(splitWord);
-// //this creates "_" for each splitWord letter 
-//     function hiddenWordArray() {
-//         hiddenArr = splitWord.map(function () {
-//             return '_';
-//             });
-//         console.log(hiddenArr);
-//         return hiddenArr;
-//     };
-//     //this calls the hiddenWordArray function
-//     hiddenWordArray();
 
-// let dashes = ""
-// let dashes_temp = ""
-// let count = 0
-// var newStr = document.getElementById('hiddenWord').innerHTML;
-// for (let i = 0; i < splitWord.length; i++) { 
-//     // dashes = dashes + "_"
-//     if (splitWord[i] === letter) {
-//         console.log("needs replacing")
-//         dashes_temp = newStr.replace(newStr.charAt[i], splitWord[i])
-//     }
-//     else {
-//     console.log("stays as dash")
-    
-//      } 
-//      console.log(newStr.charAt[i])           
-// }
-// count++ 
-// console.log(dashes_temp)
-// // var textnode = document.createTextNode(dashes_temp);
-// // var textnode2 = document.createTextNode(hiddenArr);
-// var item = document.getElementById("hiddenWord");
-// item.appendChild(textnode) 
-// item.appendChild(textnode2) 
-// item.removeChild(textnode2)  
-
-// console.log(item)  
